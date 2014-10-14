f87b88470446186d9330fa9c8a47e70f49534461
Maxime Raynal <me@maxproject.fr>
2014-10-14 13:35:50 +0200

    Mise en place du service de chargement des notes

---
+++ src/src/SimpleNote/NoteBundle/Service/PageLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SimpleNote\NoteBundle\Service;
+
+use SimpleNote\NoteBundle\Entity\Note;
+use SimpleNote\NoteBundle\Entity\Page;
+
+class PageLoader {
+
+    private $basePath;
+
+    public function($basePath) {
+        $this->basePath = $basePath;
+    }
+
+    public function loadPages(Note $note) {
+        $directoryContent = scandir($this->basePath . $note->name);
+
+        if (! $directoryContent) {
+            throw new Exception("Error Processing Request", 1);
+        }
+
+        foreach ($directoryContent as $file) {
+            $page = new Page();
+
+            $page->setOrder(split('_', $file)[0]);
+            $page->setName(split('_', $file)[1]);
+
+            $page->text = file_get_contents($file);
+
+            $page->note = $note;
+        }
+
+    }
+}
