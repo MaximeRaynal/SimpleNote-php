37a4b7ad960bc5adf1b64ad0e7c98a14454d7fd0
Maxime Raynal <me@maxproject.fr>
2014-10-14 12:32:18 +0200

    Amélioration du lancement de la VM

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# Ce fichier est lancé à l'éxécution de la machine
+# Il lance les services requis et donne un accès
 service php5-fpm start
 service nginx start
-chown -R www-data:www-data /var/www
+chmod -R 777 /var/app/logs
+chmod -R 777 /var/app/cache
 cd /var/www
 bash
