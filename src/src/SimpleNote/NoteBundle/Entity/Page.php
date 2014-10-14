68e1cb5de65d3dcc6291ce6f3402444155028d8f
Maxime Raynal <me@maxproject.fr>
2014-10-14 13:34:59 +0200

    Ajout d'un gestionnaire d'accesseur par réflexion
    http://blog.maxproject.fr/index.php?post/2014/03/14/G%C3%A9n%C3%A9ration-/-Gestion-automatique-des-accesseurs-en-PHP

--- src/src/SimpleNote/NoteBundle/Entity/Page.php
+++ src/src/SimpleNote/NoteBundle/Entity/Page.php
@@ -3,24 +3,86 @@
 namespace SimpleNote\NoteBundle\Entity;
 
 /**
- * Une page est la composante d'une note,
- * Elle PEUT être repèré par un nom
- * SINON par son numéro (ordre)
- * Elle contient du texte rédigé au format Markdown
- * Cette élément n'est pas stocké en base de données mais local
- */
+* Une page est la composante d'une note,
+* Elle PEUT être repèré par un nom
+* SINON par son numéro (ordre)
+* Elle contient du texte rédigé au format Markdown
+* Cette élément n'est pas stocké en base de données mais local
+*/
 public class Page {
 
-    private $numero;
+    private $order;
 
     private $note;
 
     private $name;
 
+    /**
+     * Texte au format Markdown
+     */
     private $text;
 
     public function __construct() {
-
     }
 
- }
+    /**
+     * Gère l'accès aux propriétés privés
+     * (remplace une longue list de Getters / Setters)
+     *
+     * Fonction géré :
+     * - get('prop') : Retourne la valeur de l'attribut
+     * - get('prop1', 'prop2', 'prop3') : (voir suivant)
+     * - get(array('prop1', 'prop2', 'prop3')) : Retourne un tableau
+     * associatif contenant les valeurs des attributs
+     * - getProp() : Retourne la valeur de l'attribut prop
+     * - set('prop', valeur) : Affecte valeur à prop
+     * - setProp(valeur) : Affecte valeur à prop
+     * - set(array('prop1' => valeur, 'prop2' => valeur))
+     * @return Si c'est un getter : la/les propriétés demandé
+     * Si c'est un setter : $this
+     */
+    public function __call($name, $arguments) {
+        $reflex = new ReflectionClass($this); //Unité de réflèxion
+        $action = '';
+        $requieredProperties = array();
+        if ($name == 'get' || $name == 'set') { //Phase de détermination
+            $action = $name;
+            if (count($arguments) == 2 && $action == 'set') {
+                $requieredProperties = array($arguments[0] => $arguments[1]);
+            }elseif (count($arguments) == 1 && is_array($arguments[0])) {
+                $requieredProperties = $arguments[0];
+            } else {
+                $requieredProperties = $arguments;
+            }
+        } elseif (strlen($name) > 3 && preg_match('/^(get|set)/', $name)) {
+            $action = substr($name, 0, 3);
+            if ($action == 'get') {
+                $requieredProperties[] = lcfirst(substr($name, 3));
+            } elseif ($action == 'set') {
+                $requieredProperties =
+                array(lcfirst(substr($name, 3)) => $arguments[0]);
+            }
+        } else { //Pas matché un getter ou setter
+            throw new Exception("Method : " . $name . ' unknow.', 1);
+        }
+        $result = array(); //Phase d'action
+        foreach ($requieredProperties as $aK => $aV) {
+            if ($reflex->hasProperty($aK) || $reflex->hasProperty($aV)) {
+                if ($action == 'get') {
+                    $property = $reflex->getProperty($aV);
+                    $property->setAccessible(true);
+                    $result[$aV] = $property->getValue($this);
+                } elseif ($action == 'set') {
+                    $property = $reflex->getProperty($aK);
+                    $property->setAccessible(true);
+                    $property->setValue($this, $aV);
+                }
+            } else {
+                throw new Exception('Property : '.$aK.'=>'.$aV.' unknow !', 42);
+            }
+        }
+        return count($result) == 0 ? $this :
+            (count($result) == 1 ? $result[$requieredProperties[0]] : $result);
+    }
+
+}
