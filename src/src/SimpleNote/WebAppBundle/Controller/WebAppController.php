f2f03a03ffcdb0c4f154c2613fe6be119b0192bb
Maxime Raynal <me@maxproject.com>
2014-10-18 17:37:02 +0200

    Renommage du Controller de la WebApp

--- src/src/SimpleNote/WebAppBundle/Controller/WebAppController.php
+++ src/src/SimpleNote/WebAppBundle/Controller/WebAppController.php
@@ -4,7 +4,7 @@ namespace SimpleNote\WebAppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class WebAppController extends Controller
 {
     public function indexAction()
     {
