b4d393e95bef1aff53edfeac9adca6d52532d280
Maxime Raynal <me@maxproject.fr>
2014-10-29 20:17:20 +0100

    Préparation de la mise en place d'angular

--- src/src/SimpleNote/WebAppBundle/Controller/DefaultController.php
+++ src/src/SimpleNote/WebAppBundle/Controller/DefaultController.php
@@ -8,6 +8,22 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SimpleNoteWebAppBundle:Default:index.html.twig');
+        return $this->render('SimpleNoteWebAppBundle:Globals:index.html.twig');
+    }
+
+    /**
+     * Cette méthode permet de récupérer les templates d'Angular JS
+     * Même si l'on force la publication dans web, l'URL Rewriting
+     * peut empêcher une bonne récupération des templates.
+     * Cette méthode capte l'appel et va chercher la vue dans le dossier
+     * Resources/public/views/ de ce Bundle et renvoi la vu correspondate
+     */
+    public function resourcesAction($resources) {
+        $prePath = dirname(__FILE__) . '/../Resources/public/views/';
+
+        // L'utilisation de realpath permet de supprimer les accès relatif (..)
+        // qui font planter file_get_contents
+        return new Response(
+                        file_get_contents(realpath($prePath . $resources)));
     }
 }
